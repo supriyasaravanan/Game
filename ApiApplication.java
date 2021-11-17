3399e16d439b905d537fe3c67734bada080927ac
supriyasaravanan <73660727+supriyasaravanan@users.noreply.github.com>
2021-11-17 23:32:44 +0530

    Add files via upload

---
+++ ApiApplication.java
@@ -0,0 +1,12 @@
+package com.localme.api.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
